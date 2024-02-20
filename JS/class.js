07c9dd0efe8f7afd7b53459c12d6c3d1559e7ef5
Akash Kumar <117556687+seakash1204@users.noreply.github.com>
2024-02-20 15:20:21 +0530

    Changes done

---
+++ JS/class.js
@@ -0,0 +1,26 @@
+// let person = {
+//     id : 101,
+//     name : "Akash",
+//     age : 28,
+//     greet() {
+//         console.log(`Hello, My name is ${this.name} and i am ${this.age} years old`)
+//     }
+// }
+// console.log(person)
+// person.id = 102;
+// console.log(person)
+// let employee = {
+//     salary : 50000,
+// }
+
+// employee.__proto__ = person;
+// console.log(employee)
+// // employee.greet()
+
+
+class Person { 
+    #id = 101
+    // console.log(#id)
+}
+// Person.id = 105;
+
